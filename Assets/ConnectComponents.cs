46559e3cba60f7569f231ba8d5b1f9c19b5287de
Sudikoff Lab iMac <imac@dhcp-212-241.cs.dartmouth.edu>
2016-05-14 22:39:43 -0400

    adding connections and visualizations; script for not-clickable

--- Assets/ConnectComponents.cs
+++ Assets/ConnectComponents.cs
@@ -8,22 +8,46 @@ public class ConnectComponents : MonoBehaviour {
 	// will have to look at rigid connections between these things, and how to fix a point and simulate
 	public GameObject connector; // cylinder to represent connector
 	public Button myselfButton;
+	private MobileMaster masterMobile;
+	public float indicator_size = 1f;
+
 
 	// Use this for initialization
 	void Start () {
 		myselfButton = GetComponent<Button>();
-		myselfButton.onClick.AddListener(() => instantiateMyCube());
+		myselfButton.onClick.AddListener(() => connect());
+
+		masterMobile = GameObject.Find("Mobile_Master").GetComponent<MobileMaster> ();
 	}
 
-	void instantiateMyCube() {
+	void connect() {
+		Debug.Log ("trying to connect");
 		// instantiate the cylinder with one of the points as point of instantiation
+		GameObject obj1 = ((GameObject)masterMobile.selected[0]);
+		GameObject obj2 = ((GameObject)masterMobile.selected[1]);
 
-		Vector3 pos = new Vector3(0, 0, 0);
+		Vector3 obj1_pos = obj1.transform.position;
+		Vector3 obj2_pos = obj2.transform.position;
+
+		Vector3 pos = Vector3.Lerp(obj1_pos, obj2_pos, 0.5f);
+
 		Quaternion rot = Quaternion.identity;
-		Instantiate (connector, pos, rot);
+		GameObject newConnector = (GameObject) Instantiate (connector, pos, rot);
+		//newConnector.transform.parent = obj1.transform; //parent CoM to the object its representing
 
-		// transform.LookAt to mke cylinder face the other point
+		Vector3 scale_vec = new Vector3(indicator_size, indicator_size, indicator_size);
+		newConnector.transform.localScale = scale_vec;
+
+		// transform.LookAt to make y axis of cylinder face the other point
+		newConnector.transform.LookAt(obj2.transform.TransformPoint(obj2_pos));
+		newConnector.transform.Rotate (new Vector3 (1.0f, 0, 0), 90);
+
 		//scale cylinder based on distance between the points
+		Vector3 newScale = newConnector.transform.localScale;
+//		newScale.y = Vector3.Distance(obj1.transform.TransformPoint(obj1_pos), obj2.transform.TransformPoint(obj2_pos))/2;
+		newScale.y = Vector3.Distance(obj1_pos, obj2_pos)/2;
+		newConnector.transform.localScale = newScale;
+
 
 	}
 
@@ -34,6 +58,6 @@ public class ConnectComponents : MonoBehaviour {
 
 	// make sure we free up the listener
 	void Destroy() {
-		myselfButton.onClick.RemoveListener(() => instantiateMyCube());
+		myselfButton.onClick.RemoveListener(() => connect());
 	}
 }
---
+++ Assets/IgnoreClicks.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class IgnoreClicks : MonoBehaviour, ICanvasRaycastFilter
+{
+	public bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+	{
+		return false;
+	}
+}
