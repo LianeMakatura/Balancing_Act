f1f62f7f890d58b6d6ac5875aba1dda7c48cb5df
gloli <gloria.y.li.16@dartmouth.edu>
2016-05-16 14:38:19 -0400

    exporting works! modified usage of OBJExport package from inspector to in-game button click. exports OBJ file of meshes

---
+++ Assets/SVGScripts/ExportSvg.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExportSvg : MonoBehaviour {
+	public Button myselfButton;
+
+	// Use this for initialization
+	void Start () {
+		myselfButton = GetComponent<Button>();
+		myselfButton.onClick.AddListener (() => Exporter.CreateWizard ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
